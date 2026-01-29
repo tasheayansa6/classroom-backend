8e71f6fd1527b9049f5ecf099ed7808fa606161e
Tashee Yansa <tasheayansa6@gmail.com>
2026-01-29 09:22:19 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+
+app.get('/', (req , res) => {
+    res.send('Hello,welcome to class API!');
+});
+
+app.listen(PORT, () => {
+    console.log(`server is running at http://localhost:${PORT}`);
+});
