5941fa901de709e63cbfcb6e8b83094309a310c9
Tashee Yansa <tasheayansa6@gmail.com>
2026-01-29 21:08:58 +0300

    implement GET all subjects route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
-import express from 'express';
 
+import cors from "cors";
+import express from 'express';
+import subjectsRouter from "./routes/subjects.js"; // lowercase 's'
 const app = express();
 const PORT = 8000;
 
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL, // React app URL
+        methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+        credentials: true, // allow cookies
+    })
+);
 app.use(express.json());
+app.use("/api/subjects", subjectsRouter);
+
 
 app.get('/', (req , res) => {
     res.send('Hello,welcome to class API!');
@@ -12,4 +23,5 @@ app.get('/', (req , res) => {
 app.listen(PORT, () => {
     console.log(`server is running at http://localhost:${PORT}`);
 });
+
 
